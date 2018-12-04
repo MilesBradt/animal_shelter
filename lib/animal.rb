0785ea463cd5258b9f6ff2fe325bb4276ce7238e
Katrina Hockman, Miles Bradt and Ngan Nguyen <ngan@>
2018-12-04 11:57:09 -0800

    add new sorting methods, not tested

--- lib/animal.rb
+++ lib/animal.rb
@@ -12,6 +12,9 @@ class Animal
     @added = attributes.fetch(:date_added)
   end
 
+
+
+
   def self.all
     animal_list = DB.exec("SELECT * FROM animals ORDER BY type;")
     animals = []
@@ -27,6 +30,36 @@ class Animal
     animals
   end
 
+  def self.sort_by_breed
+   animal_list = DB.exec("SELECT * FROM animals ORDER BY breed;")
+   animals = []
+   animal_list.each() do |animal|
+     name = animal.fetch("name")
+     gender = animal.fetch("gender")
+     type = animal.fetch("type")
+     breed = animal.fetch("breed")
+     id = animal.fetch("id").to_i()
+     added = animal.fetch("date_added")
+     animals.push(Animal.new({:name => name, :gender => gender, :type => type, :breed => breed, :id => id, :date_added => added}))
+   end
+   animals
+  end
+
+  def self.sort_by_name
+   animal_list = DB.exec("SELECT * FROM animals ORDER BY name;")
+   animals = []
+   animal_list.each() do |animal|
+     name = animal.fetch("name")
+     gender = animal.fetch("gender")
+     type = animal.fetch("type")
+     breed = animal.fetch("breed")
+     id = animal.fetch("id").to_i()
+     added = animal.fetch("date_added")
+     animals.push(Animal.new({:name => name, :gender => gender, :type => type, :breed => breed, :id => id, :date_added => added}))
+   end
+   animals
+  end
+
   def save
     DB.exec("INSERT INTO animals (name, gender, type, breed, id, date_added) VALUES ('#{@name}', '#{@gender}', '#{@type}', '#{@breed}', #{@id}, '#{@added}');")
   end
@@ -35,18 +68,5 @@ class Animal
     self.name().==(another_animal.name()).&(self.gender().==(another_animal.gender())).&(self.type().==(another_animal.type())).&(self.breed().==(another_animal.breed())).&(self.id().==(another_animal.id())).&(self.date_added().==(another_animal.date_added()))
   end
 
-  # def self.sort
-  #   ordered_list = DB.exec("SELECT * FROM animals ORDER BY type;")
-  #   animals_sorted = []
-  #   adopter_list.each() do |adopter|
-  #     name = adopter.fetch("name")
-  #     number = adopter.fetch("phone_number")
-  #     type = adopter.fetch("animal_type")
-  #     breed = adopter.fetch("animal_breed")
-  #     id = adopter.fetch("id").to_i()
-  #     adopters.push(Adopter.new({:name => name, :phone_number => number, :animal_type => type, :animal_breed => breed, :id => id}))
-  #   end
-  #   adopters
-  # end
 
 end
